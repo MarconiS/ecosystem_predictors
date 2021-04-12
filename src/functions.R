c02c4824d35f2354ffe47f2aa22ff9dd88138de8
Sergio Marconi <s.marconi@ufl.edu>
2021-04-12 14:32:34 -0400

    pipeline in main, functions in functions, extract statistics in retrieve_Stats

--- src/functions.R
+++ src/functions.R
@@ -4,19 +4,20 @@ train_model <- function(df, features, response, seed = 33, scale_responses=F){
   scaling_xs = scale(df[,colnames(df) %in% features])
   df[,colnames(df) %in% colnames(scaling_xs)] = scaling_xs
   
-  if(scale_responses == T){
-    scaling_ys = scale(df[,colnames(df) %in% response])
-    df[,colnames(df) %in% response] = scaling_ys
-  }
+  # if(scale_responses == T){
+  #   scaling_ys = scale(df[,colnames(df) %in% response])
+  #   df[,colnames(df) %in% response] = scaling_ys
+  # }
   set.seed(seed)
   train = df %>% ungroup %>% group_by(Site) %>% sample_frac(0.8)
   test = df %>% filter(!(PlotCode %in% train$PlotCode))
   if(length(response) > 1){
     #train model using bayesian lasso (horseshoe priors) to identify which indexes and bands can be reduced
     fit = brm(paste("mvbind(", paste(paste("log10(", response, ")", sep=""), collapse = ", "),
-                    ") ~ ", paste(paste("(", features, ")"), collapse = " + "), 
+                    ") ~ ", paste(paste("(", features, ")"), collapse = " + ")
+                    , 
                     #add site effect
-                    "+ (1|Site)"
+                    #"+ (1|Site)"
     ),
     # define data and grouping correlation structures
     data = train, family = student(),
@@ -24,34 +25,47 @@ train_model <- function(df, features, response, seed = 33, scale_responses=F){
     #other parameters
     chains=2, cores =2, iter = 2000)#, backend = "cmdstanr", threads = threading(2))
   }else{
-    #train model using bayesian lasso (horseshoe priors) to identify which indexes and bands can be reduced
-    fit = brm(paste(paste(paste("log10(", response, ")", sep=""), collapse = ", "),
-                    " ~ ", paste(paste("(", features, ")"), collapse = " + ") 
-                    #add site effect
-                    ,"+ (1|Site)"
-    ),
-    # define data and grouping correlation structures
-    data = train, family = student(),
-    set_prior(horseshoe()),
-    #other parameters
-    chains=2, cores =2, iter = 2000 
-    #,backend = "cmdstanr", threads = threading(1)
-    )
+    if(features[1] %like% "nm_"){
+      #train model using bayesian lasso (horseshoe priors) to identify which indexes and bands can be reduced
+      fit = brm(paste(paste(paste("log10(", response, ")", sep=""), collapse = ", "),
+                      " ~ ", paste(paste("(", features, ")"), collapse = " + ") 
+                      #add site effect
+                      #,"+ (1|Site)"
+      ),
+      # define data and grouping correlation structures
+      data = train, family = student(),
+      set_prior(horseshoe()),
+      #other parameters
+      chains=2, cores =2, iter = 2000 
+      #,backend = "cmdstanr", threads = threading(1)
+      )
+    }else{
+      fit = brm(paste(paste(paste("log10(", response, ")", sep=""), collapse = ", "),
+                      " ~ ", paste(paste("(", features, ")"), collapse = " + ") 
+                      #add site effect
+                      #,"+ (1|Site)"
+      ),
+      # define data and grouping correlation structures
+      data = train, family = student(),
+      #other parameters
+      chains=2, cores =2, iter = 2000 
+      )
+    }
   }
   #calculate the waic of the model
-  waic_fit = waic(fit)
+  loo_fit = loo(fit)
   #calculate the bayesian R2 for the model on the test set
   bR2_fit = bayes_R2(fit, newdata=test)
   #perform cross validation, predict and calculate rmse on the original scale of y
   #kcv <- kfold(fit, K = 3, seed=seed, save_fits = TRUE)
   predicts <- predict(fit,  newdata = test)
   y = test %>% select(response)
-  if(scale_responses == T){
-    y[[response]] = y[[response]] * attr(scaling_ys,"scaled:scale")[[response]]+
-      attr(scaling_ys,"scaled:center")[[response]]
-    predicts = predicts * attr(scaling_ys,"scaled:scale")[[response]]+
-      attr(scaling_ys,"scaled:center")[[response]]
-  }
+  # if(scale_responses == T){
+  #   y[[response]] = y[[response]] * attr(scaling_ys,"scaled:scale")[[response]]+
+  #     attr(scaling_ys,"scaled:center")[[response]]
+  #   predicts = predicts * attr(scaling_ys,"scaled:scale")[[response]]+
+  #     attr(scaling_ys,"scaled:center")[[response]]
+  # }
   # define a loss function
   rmse <- function(y, yrep) {
     yrep_mean <- mean(yrep)
@@ -61,25 +75,38 @@ train_model <- function(df, features, response, seed = 33, scale_responses=F){
   rmse_ft = rmse(y[[response]] , (predicts[["Estimate"]]))
   #rmse(y, colMeans(predicts))
   #return a list with model, metrics, and scaling factors
-  return(list(model = fit, bR2 = bR2_fit, WAIC = waic_fit, RMSE = rmse_ft, 
-              scaling = list(responses= scaling_ys, features = scaling_xs)))
+  return(list(model = fit, bR2 = bR2_fit, LOO = loo_fit, RMSE = rmse_ft, 
+              y_hat_test = predicts, y_test = y,
+              scaling = list(features = scaling_xs)))
   
 }
 
 #path_predictors = "~/Dropbox (UFL)/RMBL_2018/Data_for_Sergio/NEON_refl_VI_plot_cl_brnor_500_2400_adaptive.csv"
 #path_responses = "~/Dropbox (UFL)/RMBL_2018/Data_for_Sergio/Abiotic_var_fluxes_traits_field.csv"
-generate_augmented_matrix <- function(path_responses, path_predictors){
+generate_augmented_matrix <- function(path_responses, path_predictors, dim_redution =T){
   library(data.table)
   library(tidyverse)
   # want to generate on the go the dataset to feed the models into
   X = fread(path_predictors, header=T)
+  if(dim_redution ==T & path_predictors %like% "reflectance"){
+    if(path_predictors %like% "canopy"){
+      X_KD = fread("./kld_canopy.csv", header=T, drop = 1)
+    }else if(path_predictors %like% "NEON"){
+      X_KD = fread("./kld_NEON.csv", header=T, drop = 1)
+    }else if(path_predictors %like% "leaf"){
+      X_KD = fread("./kld_leaf.csv", header=T, drop = 1)
+    }
+    X = cbind.data.frame(X[["Site"]], X_KD)
+    colnames(X)[1] = "Site"
+  }
   colnames(X)
   
   #get the average predictors in plot
   X = X %>% group_by(PlotCode, Site) %>% summarize_if(is.numeric, mean)
-  X = X %>% select(-one_of(c("Site", "Year")))
+  X = X %>% ungroup %>%select(-one_of(c("Site", "Year")))
   if(path_predictors %like% "reflectance"){
     colnames(X)[-1]=paste("nm", colnames(X)[-1], sep="_")
+    
   }
   indexes = sapply(1:ncol(X), function(x) is.numeric(data.frame(X)[,x]))
   indexes = colnames(X)[indexes]
--- src/main_script.R
+++ src/main_script.R
@@ -7,32 +7,50 @@ path_predictors = "/Volumes/Data/Data_for_Sergio/"
 predictors = list.files(path_predictors) 
 predictors = predictors[!predictors %like% "ecosystem_fluxes"]
 path_responses = "/Volumes/Data/Data_for_Sergio/ecosystem_fluxes.csv"
+#create an empty list to store results for each model
 br2_overview=list()
-for(prdct in predictors[6:17]){
+for(prdct in predictors[-1]){
+  #retrieve specific data for specific model
   df = generate_augmented_matrix(path_predictors=paste(path_predictors, prdct,sep=""),
                                  path_responses=path_responses)
-  
+  #fixed responses
   ecosystem_fluxes = c("NEE","GPP","Reco","Rsoil","SOC","ET","WUE")
+  # separate Rao and CV is the model is spectral diversity
   features = c(df$numeric_predictors)
-  #plot responses correlation and distribution
-  # df$dataset %>% ungroup %>% select(ecosystem_fluxes) %>%
-  #   mutate_all(log10)%>%
-  #   ggpairs(aes(alpha = 0.4))
-  
-  #it seems that responses have a lognormal distribution. will use that for the formula
-  #joint model
-  #joint_model = train_model(df, response = ecosystem_fluxes, features = indexes)
-  
-  #individual models
-  df = df$dataset
-  all_models = list()
-  for(fl in ecosystem_fluxes){
-    all_models[[fl]]=train_model(df, features, fl, seed = 33, scale_responses=F)
+  if(prdct %like% "spectra_diversity"){
+    for(prdct in c("CV", "Rao")){
+      features = features[!features %like% "sd"]
+      features = prdct
+      #individual models
+      dataset = df$dataset
+      all_models = list()
+      for(fl in ecosystem_fluxes){
+        all_models[[fl]]=train_model(dataset, features, fl, seed = 33, scale_responses=F)
+      }
+      br2_ = list()
+      for(fl in ecosystem_fluxes){
+        br2_[[fl]] = (all_models[[fl]]$bR2)
+      }
+      br2_overview[[prdct]] = do.call(rbind.data.frame, br2_)
+      saveRDS(all_models,paste("./out/loo_mixed_eff", substr(prdct, 1, nchar(prdct)), ".rds", sep=""))
+      #for anything else than spectral diversity add all features that are in the file and apply to the specific model
+    }else{
+      features = features[!features %like% "sd"]
+      features = prdct
+      #individual models
+      dataset = df$dataset
+      all_models = list()
+      for(fl in ecosystem_fluxes){
+        all_models[[fl]]=train_model(dataset, features, fl, seed = 33, scale_responses=F)
+      }
+      br2_ = list()
+      for(fl in ecosystem_fluxes){
+        br2_[[fl]] = (all_models[[fl]]$bR2)
+      }
+      br2_overview[[prdct]] = do.call(rbind.data.frame, br2_)
+      saveRDS(all_models,paste("./out/loo_mixed_eff", substr(prdct, 1, nchar(prdct)), ".rds", sep=""))
+    }
   }
-  br2_ = list()
-  for(fl in ecosystem_fluxes){
-    br2_[[fl]] = (all_models[[fl]]$bR2)
-  }
-  br2_overview[[prdct]] = do.call(rbind.data.frame, br2_)
-  saveRDS(all_models,paste("./out/", substr(prdct, 1, nchar(prdct)-4), ".rds", sep=""))
+  
 }
+saveRDS(br2_overview,paste("./out/loo_overview_table_site.rds", sep=""))
